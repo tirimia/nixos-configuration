dee48ec9411929e14e360d5f39b2964582785be7
Theodor-Alexandru Irimia <Theodor.Irimia@schuettflix.com>
2023-11-21 23:15:58 +0100

    Finish zsh config

--- config/software/zsh/default.nix
+++ config/software/zsh/default.nix
@@ -14,7 +14,14 @@
         enableCompletion = true;
         enableAutosuggestions = true;
         enableSyntaxHighlighting = true;
+        history = {
+          save = 1000000;
+          size = 1000000;
+        };
         dotDir = ".config/zsh";
+        shellAliases = {
+          cdr = "cd `git rev-parse --show-toplevel`";
+        };
         initExtra = ''
           if [[ -r "$XDG_CACHE_HOME/p10k-instant-prompt-*.zsh" ]]; then
             source "$XDG_CACHE_HOME/p10k-instant-prompt-*.zsh"
@@ -28,8 +35,36 @@
           setopt always_to_end
           setopt prompt_subst
           setopt share_history
-          alias cdr='cd `git rev-parse --show-toplevel`'
-              source ~/.config/zsh/powerlevel10k.zsh
+          # Make the goddamn delete char work
+          bindkey  "^[[3~"          delete-char
+          bindkey  "^[3;5~"         delete-char
+          bindkey  ";3D"         backward-word
+          bindkey  ";3C"         forward-word
+          # Emacs vterm
+          vterm_printf(){
+              if [ -n "$TMUX" ] && ([ "''${TERM%%-*}" = "tmux" ] || [ "''${TERM%%-*}" = "screen" ] ); then
+                  # Tell tmux to pass the escape sequences through
+                  printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+              elif [ "$${TERM%%-*}" = "screen" ]; then
+                  # GNU screen (screen, screen-256color, screen-256color-bce)
+                  printf "\eP\e]%s\007\e\\" "$1"
+              else
+                  printf "\e]%s\e\\" "$1"
+              fi
+          }
+          vterm_cmd() {
+              local vterm_elisp
+              vterm_elisp=""
+              while [ $# -gt 0 ]; do
+                  vterm_elisp="$vterm_elisp""$(printf '"%s" ' "$(printf "%s" "$1" | sed -e 's|\\|\\\\|g' -e 's|"|\\"|g')")"
+                  shift
+              done
+              vterm_printf "51;E$vterm_elisp"
+          }
+          ff() {
+              vterm_cmd find-file "$(realpath "''${@:-.}")"
+          }
+          source ~/.config/zsh/powerlevel10k.zsh
         '';
         plugins = [
           {
