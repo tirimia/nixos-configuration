99e011aaf56fe641ea280c6ac089b171152598a7
Theodor Irimia <Thetheodor.irimia@gmail.com>
2024-05-21 22:03:25 +0200

    Add 1password

--- users/tirimia/default.nix
+++ users/tirimia/default.nix
@@ -24,6 +24,13 @@ in {
     shell = pkgs.zsh;
   };
   programs.zsh.enable = true;
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [username];
+  };
 
   home-manager = {
     useUserPackages = true;
@@ -35,6 +42,7 @@ in {
         packages = with pkgs; [
           megasync # TODO: run as a service
           _1password
+          _1password-gui
         ];
       };
       programs.home-manager.enable = true;
